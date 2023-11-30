7b37957ba9577547180b43a7be3604cea860e936
mitul88 <shahed.rahman88@gmail.com>
2023-11-30 23:36:07 +0600

    navbar responsiveness done

--- src/components/SiteNavbar.jsx
+++ src/components/SiteNavbar.jsx
@@ -5,7 +5,45 @@ import {usePathname} from "next/navigation";
 
 const SiteNavbar = () => {
 
-    const NavClick = () => {}
+    const NavClick = () => {
+        // open
+        const burger = document.querySelectorAll('.navbar-burger');
+        const menu = document.querySelectorAll('.navbar-menu');
+
+        if (burger.length && menu.length) {
+            for (var i = 0; i < burger.length; i++) {
+                burger[i].addEventListener('click', function() {
+                    for (var j = 0; j < menu.length; j++) {
+                        menu[j].classList.toggle('hidden');
+                    }
+                });
+            }
+        }
+
+        // close
+        const close = document.querySelectorAll('.navbar-close');
+        const backdrop = document.querySelectorAll('.navbar-backdrop');
+
+        if (close.length) {
+            for (var i = 0; i < close.length; i++) {
+                close[i].addEventListener('click', function() {
+                    for (var j = 0; j < menu.length; j++) {
+                        menu[j].classList.toggle('hidden');
+                    }
+                });
+            }
+        }
+
+        if (backdrop.length) {
+            for (var i = 0; i < backdrop.length; i++) {
+                backdrop[i].addEventListener('click', function() {
+                    for (var j = 0; j < menu.length; j++) {
+                        menu[j].classList.toggle('hidden');
+                    }
+                });
+            }
+        }
+    }
 
     const current=usePathname();
   
@@ -49,6 +87,42 @@ return (
                 </li>
             </ul>
         </nav>
+
+
+        <div className='hidden navbar-menu fixed top-0 left-0 bottom-0 w-5/6 max-w-sm z-50'>
+                <div onClick={()=>{NavClick()}} className='navbar-backdrop  fixed inset-0 bg-gray-800 opacity-25' />
+                <nav className='relative flex flex-col py-6 px-6 h-full w-full bg-white border-r overflow-y-auto'>
+                    <div className='flex items-center mb-8'>
+                        <a className='mr-auto text-3xl font-bold leading-none' href='#'>
+                            <img className='h-10' src='atis-assets/logo/atis/atis-mono-black.svg' alt='' width='auto'/>
+                        </a>
+                        <button onClick={()=>{NavClick()}} className='navbar-close'>
+                            <svg className='h-6 w-6 text-gray-400 cursor-pointer hover:text-gray-500' xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke='currentColor'>
+                                <path strokeLinecap='round' strokeLinejoin='round' strokeWidth={2} d='M6 18L18 6M6 6l12 12'/>
+                            </svg>
+                        </button>
+                    </div>
+                    <div>
+                        <ul>
+                            <li className='mb-1'>
+                                <Link className='block p-4 text-sm font-semibold text-gray-400 hover:bg-green-50 hover:text-green-600 rounded' href={'/'}>Start</Link>
+                            </li>
+                            <li className='mb-1'>
+                                <Link className='block p-4 text-sm font-semibold text-gray-400 hover:bg-green-50 hover:text-green-600 rounded' href={'/team'}>Team</Link>
+                            </li>
+                            <li className='mb-1'>
+                                <Link className='block p-4 text-sm font-semibold text-gray-400 hover:bg-green-50 hover:text-green-600 rounded' href={'/services'}>Services</Link>
+                            </li>
+                            <li className='mb-1'>
+                                <Link className='block p-4 text-sm font-semibold text-gray-400 hover:bg-green-50 hover:text-green-600 rounded' href={'/projects'}>Projects</Link>
+                            </li>
+                            <li className='mb-1'>
+                                <Link className='block p-4 text-sm font-semibold text-gray-400 hover:bg-green-50 hover:text-green-600 rounded' href={'/testimonials'}>Testimonials</Link>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
     </div>
   )
 }

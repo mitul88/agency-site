e00f653f11e2ef63670821ae3cf91edabc4f9eae
mitul88 <shahed.rahman88@gmail.com>
2023-12-01 11:57:14 +0600

    footer done

--- src/components/SiteFooter.jsx
+++ src/components/SiteFooter.jsx
@@ -2,13 +2,16 @@ import Link from 'next/link'
 import React from 'react'
 
 const SiteFooter = () => {
+
+    const date = new Date();
+    const currentYear = date.getFullYear();
   return (
     <footer className='bg-black text-white p-20'>
-        <div className="flex flex-col lg:flex-row justify-between items-center">
-            <div className="w-full lg:w-1/5">
-                <h1 className="text-3xl font-bold">Web Logo</h1>
-                <p className="text-lg">Some Footer text about the agency. Just a little description to help people understand you better.</p>
-                <div className='flex justify-between w-48'>
+        <div className="flex flex-col md:flex-row justify-between items-center">
+            <div className="w-full md:w-1/5 mb-10 md:mb-0">
+                <h1 className="text-2xl lg:text-3xl font-bold mb-5">Web Logo</h1>
+                <p className="text-sm md:text-md lg:text-lg">Some Footer text about the agency. Just a little description to help people understand you better.</p>
+                <div className='flex justify-between w-48 mt-3'>
                     <div className="flex flex-col items-center justify-center bg-[#20B15A] rounded-full px-4 py-3">
                         <img src="/facebook.png" alt="" />
                     </div>
@@ -20,9 +23,9 @@ const SiteFooter = () => {
                     </div>
                 </div>
             </div>
-            <div className="w-full lg:w-1/5">
-                <h3 className="text-lg font-semibold">Quick Links</h3>
-                <ul>
+            <div className="w-full md:w-1/5 mb-10 md:mb-0">
+                <h3 className="text-md lg:text-lg font-semibold mb-3">Quick Links</h3>
+                <ul className='w-full flex justify-between md:flex-col'>
                     <li className='mb-1'>
                         <Link className='block' href={'/'}>Start</Link>
                     </li>
@@ -40,12 +43,12 @@ const SiteFooter = () => {
                     </li>
                 </ul>
             </div>
-            <div className="w-full lg:w-1/5">
-                <h3 className="text-lg font-semibold">Address</h3>
-                <p>Design agency head office <br /> Airport Road <br /> United Arab Emirates</p>
+            <div className="w-full md:w-1/5 mb-10 md:mb-0">
+                <h3 className="text-md lg:text-lg font-semibold mb-3">Address</h3>
+                <p className='text-sm md:text-md lg:text-lg'>Design agency head office <br /> Airport Road <br /> United Arab Emirates</p>
             </div>
         </div>
-        <h4 className="my-5 text-lg">Copyright Design Agency 2022</h4>
+        <h4 className="mt-5 text-md lg:text-lg">Copyright Design Agency {currentYear}</h4>
     </footer>
   )
 }
